07d5d8d2f852208975e8ffdc7bce27fbf2b2db71
Samel Martinucci <samuelmartinucci@gmail.com>
2015-05-07 17:14:12 -0300

    Championship route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   match '/contacts',     to: 'contacts#new',             via: 'get'
   resources "contacts", only: [:new, :create]
 
+  resources :championships
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
